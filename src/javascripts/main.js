3cdeac9386aa32ece4f1329b75a9dca752161ca8
Nikaila Bell <Nikailabell@mail.weber.edu>
2025-12-11 19:24:50 -0700

    Final Project

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,14 @@ require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require("../stylesheets/main.scss")
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+// Simple form alert
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you! Your rental request has been submitted.");
+      form.reset();
+    });
+  }
+});
